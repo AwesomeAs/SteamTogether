8345c1d4d24b30dbf4355a559de08644ceb6f74e
AwesomeAs <bloxxer9001@gmail.com>
2018-03-25 16:45:17 +0200

    Config added

---
+++ Config.php
@@ -0,0 +1,9 @@
+<?php
+	$dbconfig = array(
+		"host" => "YOUR SQL SERVER HOST",
+		"port" => 1234,
+		"username" => "YOUR SQL SERVER LOGIN USERNAME",
+		"password" => "YOUR SQL SERVER LOGIN PASSWORD",
+		"database" => "YOUR SQL SERVER DATABASE NAME"
+	);
+?>
